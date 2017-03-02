1c25a46778337ec4e04829ddf8e475cc1a3dee73
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2017-03-02 11:40:06 +0100

    TableSchemaException added.

---
+++ src/Tables/TableSchemaException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
+ * @copyright 2010-2017 JTL-Software GmbH
+ */
+
+namespace jtl\Connector\CDBC\Tables;
+
+
+class TableSchemaException extends \Exception
+{
+
+}
