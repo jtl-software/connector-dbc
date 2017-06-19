765bfec305d963221a68e4818ff01d8f558f65de
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2017-06-19 11:53:19 +0200

    Host index changed from unique to normal index.

--- src/Mapping/AbstractMappingTable.php
+++ src/Mapping/AbstractMappingTable.php
@@ -42,7 +42,7 @@ abstract class AbstractMappingTable extends AbstractTable implements MappingTabl
     {
         $tableSchema = parent::getTableSchema();
         $tableSchema->addColumn(self::HOST_ID, Type::INTEGER, ['notnull' => false]);
-        $tableSchema->addUniqueIndex([self::HOST_ID], self::HOST_INDEX_NAME);
+        $tableSchema->addIndex([self::HOST_ID], self::HOST_INDEX_NAME);
         return $tableSchema;
     }
 
