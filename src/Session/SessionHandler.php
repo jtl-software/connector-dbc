86ac599a1e6b7de0a22dda2463e7b98bce25edca
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2020-05-20 13:49:00 +0200

    Added php session handler

---
+++ src/Session/SessionHandler.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Jtl\Connector\Dbc\Session;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Exception\InvalidArgumentException;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\DBAL\Types\Types;
+use Jtl\Connector\Dbc\AbstractTable;
+use Jtl\Connector\Dbc\DbManager;
+
+class SessionHandler extends AbstractTable implements \SessionHandlerInterface
+{
+    public const
+        SESSION_ID = 'session_id',
+        SESSION_DATA = 'session_data',
+        EXPIRES_AT = 'expires_at';
+
+    /**
+     * @var string
+     */
+    protected $tableName;
+
+    /**
+     * @var int
+     */
+    protected $maxLifetime;
+
+    /**
+     * SessionHandler constructor.
+     * @param DbManager $dbManager
+     * @param string $tableName
+     * @throws \Exception
+     */
+    public function __construct(DbManager $dbManager, string $tableName = 'session_store')
+    {
+        $this->tableName = $tableName;
+        $this->maxLifetime = (int) ini_get('session.gc_maxlifetime');
+        parent::__construct($dbManager);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getName(): string
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @param Table $tableSchema
+     */
+    protected function createTableSchema(Table $tableSchema): void
+    {
+        $tableSchema->addColumn(self::SESSION_ID, Types::STRING, ['length' => 128]);
+        $tableSchema->addColumn(self::SESSION_DATA, Types::BLOB);
+        $tableSchema->addColumn(self::EXPIRES_AT, Types::DATETIME_IMMUTABLE);
+        $tableSchema->setPrimaryKey([self::SESSION_ID]);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function close()
+    {
+        return true;
+    }
+
+    /**
+     * @param string $sessionId
+     * @return bool
+     * @throws DBALException
+     * @throws InvalidArgumentException
+     */
+    public function destroy($sessionId)
+    {
+        $this->delete([self::SESSION_ID => $sessionId]);
+        return true;
+    }
+
+    /**
+     * @param int $maxLifetime
+     * @return bool
+     */
+    public function gc($maxLifetime)
+    {
+        $this->createQueryBuilder()
+            ->delete($this->getTableName())
+            ->andWhere($this->getConnection()->getExpressionBuilder()->lt(self::EXPIRES_AT, sprintf(':%s', 'now')))
+            ->setParameter('now', new \DateTimeImmutable(), Types::DATETIME_IMMUTABLE)
+            ->execute();
+
+        return true;
+    }
+
+    /**
+     * @param string $savePath
+     * @param string $name
+     * @return boolean
+     */
+    public function open($savePath, $name)
+    {
+        return true;
+    }
+
+    /**
+     * @param string $sessionId
+     * @return false|mixed|string
+     */
+    public function read($sessionId)
+    {
+        $stmt = $this->createQueryBuilder()
+            ->select(self::SESSION_DATA)
+            ->from($this->getTableName())
+            ->where($this->getConnection()->getExpressionBuilder()->eq(self::SESSION_ID, ':sessionId'))
+            ->setParameter('sessionId', $sessionId)
+            ->andWhere($this->getConnection()->getExpressionBuilder()->gt(self::EXPIRES_AT, sprintf(':%s', 'now')))
+            ->setParameter('now', new \DateTimeImmutable(), Types::DATETIME_IMMUTABLE)
+            ->execute();
+
+        if ($stmt instanceof \PDOStatement) {
+            return (string)$stmt->fetchColumn();
+        }
+        return '';
+    }
+
+    /**
+     * @param string $sessionId
+     * @param string $sessionData
+     * @return bool
+     * @throws DBALException
+     */
+    public function write($sessionId, $sessionData)
+    {
+        $data = [
+            self::SESSION_DATA => $sessionData,
+            self::EXPIRES_AT => (new \DateTimeImmutable())->setTimestamp(time() + $this->maxLifetime)
+        ];
+
+        $rowCount = $this->update($data, [self::SESSION_ID => $sessionId]);
+        if ($rowCount === 0) {
+            $data[self::SESSION_ID] = $sessionId;
+            $this->insert($data);
+        }
+
+        return true;
+    }
+}
---
+++ tests/src/Session/SessionHandlerTest.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Jtl\Connector\Dbc\Session;
+
+use Jtl\Connector\Dbc\AbstractDbTestCase;
+use Jtl\Connector\Dbc\DbManager;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class SessionHandlerTest extends AbstractDbTestCase
+{
+    protected $handler;
+
+    protected function setUp(): void
+    {
+        $this->handler = new SessionHandler($this->getDBManager());
+        parent::setUp();
+    }
+
+    /**
+     * @runInSeparateProcess
+     *
+     * @throws \ReflectionException
+     */
+    public function testMaxLifetime()
+    {
+        $expected = 254;
+        ini_set('session.gc_maxlifetime', $expected);
+        $handler = new SessionHandler($this->createMock(DbManager::class));
+        $reflection = new \ReflectionClass($handler);
+        $reflMaxLifetimeProp = $reflection->getProperty('maxLifetime');
+        $reflMaxLifetimeProp->setAccessible(true);
+        $this->assertEquals($expected, $reflMaxLifetimeProp->getValue($handler));
+    }
+
+    public function testReadSessionValid()
+    {
+        $sessionId = uniqid('sess', true);
+        $sessionData = 'serializedSessionData';
+
+        $data = [
+            SessionHandler::SESSION_ID => $sessionId,
+            SessionHandler::SESSION_DATA => $sessionData,
+            SessionHandler::EXPIRES_AT => (new \DateTimeImmutable())->setTimestamp(time() + 1)
+        ];
+
+        $this->handler->insert($data);
+        $actual = $this->handler->read($sessionId);
+        $this->assertEquals($sessionData, $actual);
+    }
+
+    public function testReadSessionExpired()
+    {
+        $sessionId = uniqid('sess', true);
+        $sessionData = 'something';
+
+        $data = [
+            SessionHandler::SESSION_ID => $sessionId,
+            SessionHandler::SESSION_DATA => $sessionData,
+            SessionHandler::EXPIRES_AT => (new \DateTimeImmutable())->setTimestamp(time() - 1)
+        ];
+
+        $this->handler->insert($data);
+
+        $actual = $this->handler->read($sessionId);
+        $this->assertEquals('', $actual);
+    }
+
+    public function testWriteInsert()
+    {
+        $sessionId = uniqid('sess', true);
+        $sessionData = 'serializedSessionData';
+
+        $this->assertTableRowCount($this->handler->getTableName(), 0);
+        $this->handler->write($sessionId, $sessionData);
+        $this->assertTableRowCount($this->handler->getTableName(), 1);
+    }
+
+    public function testWriteUpdate()
+    {
+        $sessionId = uniqid('sess', true);
+        $sessionData = 'yeasdasdasf';
+
+        $data = [
+            SessionHandler::SESSION_ID => $sessionId,
+            SessionHandler::SESSION_DATA => $sessionData,
+            SessionHandler::EXPIRES_AT => (new \DateTimeImmutable())->setTimestamp(time() + 1)
+        ];
+
+        $this->handler->insert($data);
+        $this->assertEquals($sessionData, $this->handler->read($sessionId));
+        $newData = 'yalla';
+        $this->handler->write($sessionId, $newData);
+        $this->assertEquals($newData, $this->handler->read($sessionId));
+    }
+
+    public function testClose()
+    {
+        $this->assertTrue($this->handler->close());
+    }
+
+    public function testOpen()
+    {
+        $this->assertTrue($this->handler->open('yalla', 'yolo'));
+    }
+
+    public function testDestroy()
+    {
+        $sessionId = uniqid('sess', true);
+        $sessionData = 'something';
+
+        $data = [
+            SessionHandler::SESSION_ID => $sessionId,
+            SessionHandler::SESSION_DATA => $sessionData,
+            SessionHandler::EXPIRES_AT => (new \DateTimeImmutable())->setTimestamp(time() - 1)
+        ];
+
+        $this->assertTableRowCount($this->handler->getTableName(), 0);
+        $this->handler->insert($data);
+        $this->assertTableRowCount($this->handler->getTableName(), 1);
+        $this->handler->destroy($sessionId);
+        $this->assertTableRowCount($this->handler->getTableName(), 0);
+    }
+
+    public function testGc()
+    {
+        $expiredCount = 0;
+        $insertedRows = mt_rand(3, 10);
+        for ($i = 0; $i < $insertedRows; $i++) {
+            $expiresAt = (new \DateTimeImmutable())->setTimestamp(time() + 1);
+            if (mt_rand(0, 1) === 1) {
+                $expiresAt = (new \DateTimeImmutable())->setTimestamp(time() - 1);
+                $expiredCount++;
+            }
+
+            $this->handler->insert([
+                SessionHandler::SESSION_ID => uniqid('sess', true),
+                SessionHandler::SESSION_DATA => sprintf('round %s', $i),
+                SessionHandler::EXPIRES_AT => $expiresAt
+            ]);
+        }
+
+        $this->assertTableRowCount($this->handler->getTableName(), $insertedRows);
+        $this->handler->gc(1234);
+        $this->assertTableRowCount($this->handler->getTableName(), $insertedRows - $expiredCount);
+    }
+}
